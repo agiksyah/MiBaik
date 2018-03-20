52534a56a4d59de6eddee1d42f314b9d081f2266
agiksyah <agnosgix@gmail.com>
2018-03-20 11:00:07 +0700

    changetapp

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const PRECACHE = 'precahce-v1';
+/*const PRECACHE = 'precahce-v1';
 const RUNTIME = 'runtime';
 
 const PRECACHE_URLS = [
@@ -10,6 +10,7 @@ const PRECACHE_URLS = [
 	'css/style.css',
 	'js/app.js',
 	'js/progressbar.js',
+	'sound/beep.wav',
 	'manifest.json'
 ]
 
@@ -40,3 +41,48 @@ self.addEventListener('fetch', function (event) {
 self.addEventListener('push', function (event) {
 	console.log("[ServiceWorker] Fetching", event.request.url);
 });
+*/
+
+var cacheName = 'cache-v1';
+var cacheFiles = [
+	'/',
+	'index.html',
+	'index.html?homescreen=1',
+	'?homescreen=1',
+	'https://fonts.googleapis.com/css?family=Orbitron',
+	'css/style.css',
+	'js/app.js',
+	'js/progressbar.js',
+	'sound/beep.wav',
+	'manifest.json'
+]
+
+self.addEventListener('install', function(event) {
+	console.log("[ServiceWorker] Installed")
+
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache){
+			console.log("[ServiceWorker] Caching cacheFiles");
+			return cache.addAll(cacheFiles);
+		})
+	)
+})
+
+self.addEventListener('activate', function(event) {
+	console.log("[ServiceWorker] Activated")
+
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(cacheNames.map(function(thisCacheName) {
+				if (thisCacheName !== cacheName) {
+				console.log("[ServiceWorker] Removing Cached Files From", thisCacheName);
+				return caches.delete(thisCacheName);	
+				}
+			}))
+		})
+	)
+})
+
+self.addEventListener('fetch', function(event) {
+	console.log("[ServiceWorker] Fetching", event.request.url);
+})
